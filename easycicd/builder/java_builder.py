76161ae3195b6eedc79804cff54629998423175c
zhipeng.su <zhipeng.su@rootcloud.com>
2021-09-21 15:36:54 +0800

    fix

--- easycicd/builder/java_builder.py
+++ easycicd/builder/java_builder.py
@@ -13,8 +13,8 @@ class Builder(BuilderBase):
         # tag = os.environ.get("registry") + "/" + self.root_path.split("/")[1] + ":" + str(time.time()).split(".")[0]
         tag = "registry.rootcloud.com/devops" + "/" + self.root_path.split("/")[1] + ":" + str(time.time()).split(".")[0]
         env = app_config(self.root_path)
-
-        with open("multi-stage-dockerfile/Dockerfile.java", "r") as fileobj:
+        print("&&&&&&&&&&&&&&&&&&&&&, flag1")
+        with open("easycicd/builder/multi-stage-dockerfile/Dockerfile.java", "r") as fileobj:
             image, build_log = self.client.images.build(fileobj=fileobj, tag=tag, target='BUILD', rm=True)
 
         return image, env, build_log
